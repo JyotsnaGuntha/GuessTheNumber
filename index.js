a944c5dad0a161ba0a8c777a335d0d5355ef706f
Jyotsna <147334973+JyotsnaGuntha@users.noreply.github.com>
2024-08-05 20:15:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let gameResult = document.getElementById("gameResult");
 let userInput = document.getElementById("userInput");
 let randomNumber = Math.ceil(Math.random() * 100);
+let attemptsCountElement = document.getElementById("attemptsCount");
+
+attemptsCountElement.textContent = 0;
 
 function checkGuess() {
     let guessedNumber = parseInt(userInput.value);
@@ -8,9 +11,11 @@ function checkGuess() {
     if (guessedNumber > randomNumber) {
         gameResult.textContent = "Too High! Try Again.";
         gameResult.style.backgroundColor = "#1e217c";
+        attemptsCountElement.textContent = parseInt(attemptsCountElement.textContent) + 1;
     } else if (guessedNumber < randomNumber) {
         gameResult.textContent = "Too Low! Try Again.";
         gameResult.style.backgroundColor = "#1e217c";
+        attemptsCountElement.textContent = parseInt(attemptsCountElement.textContent) + 1;
     } else if (guessedNumber === randomNumber) {
         gameResult.textContent = "Congratulations! You got it right.";
         gameResult.style.backgroundColor = "green";
